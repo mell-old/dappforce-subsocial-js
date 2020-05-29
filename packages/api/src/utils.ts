1e941dac54b61f74ae42720557565388899eab28
olehmell <oleh.melnechyk@gmail.com>
2020-05-29 20:01:28 +0300

    Fix import

--- packages/api/src/utils.ts
+++ packages/api/src/utils.ts
@@ -1,5 +1,5 @@
 import { CommentExt } from '@subsocial/types/substrate/classes';
-import { IpfsCid, SubstrateId, AnyAccountId, CommonStruct, PostData } from '@subsocial/types';
+import { IpfsCid, SubstrateId, AnyAccountId, CommonStruct } from '@subsocial/types';
 import { newLogger, isEmptyArray, nonEmptyStr } from '@subsocial/utils';
 import { PostId, AccountId, ReactionId, SocialAccount, Reaction } from '@subsocial/types/substrate/interfaces';
 import registry from '@subsocial/types/substrate/registry';
